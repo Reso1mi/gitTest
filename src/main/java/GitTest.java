2ff8140ca4b1fee98add90a7ba5a10908bf45593
lgw <privateli@qq.com>
2019-06-10 16:32:37 +0800

    dev commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,6 +4,7 @@
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("master 分支");
+        System.out.println("dev 分支");
+        System.out.println("dev 分支");
     }
 }
